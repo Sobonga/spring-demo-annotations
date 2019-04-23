86441871919634c2c9e483bba91289bf7e8847e4
Sobz <sobongaD@qode.healthcare>
2019-04-23 13:09:18 +0200

    Different Annotations

--- src/com/luv2code/springdemo/TennisCoach.java
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -2,18 +2,34 @@ package com.luv2code.springdemo;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TennisCoach implements Coach{
 
+    @Autowired
+    @Qualifier("randomFortuneService")
     private FortuneService fortuneService;
 
-    @Autowired
-    public TennisCoach(FortuneService theFortuneService){
-        fortuneService = theFortuneService;
+    //define a default constructor
+    public TennisCoach(){
+        System.out.println ( ">> TennisCoach: insert default constructor" );
     }
 
+    //define a setter method
+
+/*@Autowired
+    public void doSomeCrazyStuff(FortuneService theFortuneService) {
+    System.out.println ( ">> TennisCoach: insert doSomeCrazyStuff() method" );
+        this.fortuneService = theFortuneService;
+    }*/
+
+    /* @Autowired
+        public TennisCoach(FortuneService theFortuneService){
+            fortuneService = theFortuneService;
+        }
+    */
     @Override
     public String getDailyWorkout(){
         return "Practise your backhand volley";
